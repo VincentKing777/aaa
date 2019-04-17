6f452594d5b6b71f0ee4f7a7f84fd85dc33d51d3
VincentKing777 <wrz77888597918@gmail.com>
2019-04-17 13:19:34 -0700

    a

--- server.js
+++ server.js
@@ -1,36 +1,68 @@
 const express = require('express');
-const path = require('path');
 const hbs = require('hbs');
 const fs = require('fs');
 
 const port = process.env.PORT || 8080;
 
-var index = require('./routes/index');
-
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
+// app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname + '/public'));
-
-app.use('/', index);
-
-app.use(function (req, res, next) {
-    res.status(404);
-    res.render('404');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
 });
 
-// error handler
-app.use(function (err, req, res) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = err;
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+hbs.registerHelper('message', (text) => {
+    return text.toUpperCase();
 });
+
+// app.use((request, response, next) => {
+//     // var time = new Date().toString();
+//     // // console.log(`${time}: ${request.method} ${request.url}`);
+//     // var log = `${time}: ${request.method} ${request.url}`;
+//     // fs.appendFile('server.log', log + '\n', (error) => {
+//     //     if (error) {
+//     //         console.log('Unable to log message');
+//     //     }
+//     // });
+//     // next();
+//     response.render('maintenance.hbs')
+// });
+
+
+
+app.get('/', (request, response) => {
+    // response.send('<h1>Hello Express!</h1>');
+    response.send({
+        name: 'Your Name',
+        school: [
+            'BCIT',
+            'SFU',
+            'UBC'
+        ]
+    })
+});
+
+app.get('/info', (request, response) => {
+    response.render('about.hbs', {
+        title: 'About page',
+        // year: new Date().getFullYear(),
+        welcome: 'Hello!'
+    })
+});
+
+app.get('/404', (request, response) => {
+    response.send({
+        error: 'Page not found'
+    })
+});
+
+// app.listen(8080, () => {
+//     console.log('Server is up on the port 8080');
+// });
 
 app.listen(port, () => {
     console.log('Server is up on the port 8080');
